e0ecc6736a7bac3aa29ab1d287b80d87e6f76834
Patrick A <akolo@hotmail.com>
2019-04-28 09:31:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,10 +16,9 @@ class Scraper
         course.description = post.css ("p").text
     end
   end 
-      binding.pry 
 end 
 
-Scraper.new.get_page
+Scraper.new.get_page\
   
 #     def print_courses
 #     self.make_courses
